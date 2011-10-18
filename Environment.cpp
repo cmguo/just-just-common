67039e0debc0808026efa85a3bd806a0f55767bd
isxxguo <isxxguo@pptv.com>
2011-10-18 05:16:12 +0000

    /ppbox/branches/0.5.3 合并 /ppbox/branches/0.5.3:r393,456,482,484-485,487,492 合并 /ppbox/branches/0.5.3_improve:r733,755,797,799,897,940
    git-svn-id: http://svn01/repositories/r01/ppbox/branches/0.5.3@1037 b3a1389e-2159-5c43-9038-463bf877d0d9

--- Environment.cpp
+++ Environment.cpp
@@ -20,7 +20,8 @@ namespace ppbox
             std::vector<Interface> infs;
             if (!enum_interface(infs)) {
                 for (size_t i = 0; i < infs.size(); ++i) {
-                    if (std::string(infs[i].name) == "lo")
+                    if (std::string(infs[i].name) == "lo"
+                        || infs[i].hard_addr_string(':', false) == "00:00:00:00:00:00")
                         continue;
                     if (::memcmp(infs[i].hwaddr, "\0\0\0\0\0\0", 6) == 0)
                         continue;
