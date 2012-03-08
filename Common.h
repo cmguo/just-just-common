d7a5e41b5851a673c359c6d27201b71a640d677b
isxxguo <isxxguo@pptv.com>
2012-03-08 05:38:05 +0000

    /ppbox/branches/1.2.0 合并 /ppbox/branches/1.0.0:r2606-2875
    git-svn-id: http://svn01/repositories/r01/ppbox/branches/1.2.0@2884 b3a1389e-2159-5c43-9038-463bf877d0d9

--- Common.h
+++ Common.h
@@ -3,6 +3,10 @@
 #define FRAMEWORK_MESSAGEQUEUE_SINGLE_PROCESS
 #endif
 
+#if (!defined(PPBOX_DISABLE_PEER) && !defined(PPBOX_DISABLE_VOD))
+#define PPBOX_DISABLE_PEER
+#endif
+
 #include <util/Util.h>
 
 #include "ppbox/common/CommonModuleBase.h"
