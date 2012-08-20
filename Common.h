558132ccdce557ad06f7d76d71741b9ef8264547
isxxguo <isxxguo@pptv.com>
2012-08-20 09:02:17 +0000

    /ppbox/branches/1.2.0 src/ppbox 将Source，Segments独立到data模块
    git-svn-id: http://svn01/repositories/r01/ppbox/branches/1.2.0@4574 b3a1389e-2159-5c43-9038-463bf877d0d9

--- Common.h
+++ Common.h
@@ -1,7 +1,4 @@
 // Common.h
-#if defined( PPBOX_SINGLE_PROCESS )
-#define FRAMEWORK_MESSAGEQUEUE_SINGLE_PROCESS
-#endif
 
 #if (!defined(PPBOX_DISABLE_PEER) && !defined(PPBOX_DISABLE_VOD))
 #define PPBOX_DISABLE_PEER
