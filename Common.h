909c0e1dce1f68224d49a48ee324a3bea7029b18
isxxguo <isxxguo@pptv.com>
2012-09-25 03:47:59 +0000

    /ppbox/branches/1.2.0 /src/ppbox/common/Common.h 删除PPBOX_DISABLE_VOD
    git-svn-id: http://svn01/repositories/r01/ppbox/branches/1.2.0@4992 b3a1389e-2159-5c43-9038-463bf877d0d9

--- Common.h
+++ Common.h
@@ -1,9 +1,5 @@
 // Common.h
 
-#if (!defined(PPBOX_DISABLE_PEER) && !defined(PPBOX_DISABLE_VOD))
-#define PPBOX_DISABLE_PEER
-#endif
-
 #include <util/Util.h>
 
 #include "ppbox/common/CommonModuleBase.h"
