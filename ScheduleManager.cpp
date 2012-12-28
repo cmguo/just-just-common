aa0080a3981408345589e7d91255de09decbf1e5
isxxguo <isxxguo@pptv.com>
2012-12-28 05:24:34 +0000

    /ppbox/branches/1.2.0 /src/ppbox 增加 common/ScheduleManager 模块，代替IPpbox直接提供的ScheduleCallback功能
    git-svn-id: http://svn01/repositories/r01/ppbox/branches/1.2.0@6054 b3a1389e-2159-5c43-9038-463bf877d0d9

---
+++ ScheduleManager.cpp
@@ -0,0 +1,52 @@
+// ScheduleManager.cpp
+
+#include "ppbox/common/Common.h"
+#include "ppbox/common/ScheduleManager.h"
+
+#include <boost/asio/deadline_timer.hpp>
+#include <boost/bind.hpp>
+
+namespace ppbox
+{
+    namespace common
+    {
+
+        ScheduleManager::ScheduleManager(
+            util::daemon::Daemon & daemon)
+            : util::daemon::ModuleBase<ScheduleManager>(daemon, "ScheduleManager")
+        {
+        }
+
+        boost::system::error_code ScheduleManager::startup()
+        {
+            boost::system::error_code ec;
+            return ec;
+        }
+
+        // Í£Ö¹ÐÄÌø(keepalive)
+        void ScheduleManager::shutdown()
+        {
+        }
+
+        void * ScheduleManager::schedule_callback(
+            boost::uint32_t delay, // ms
+            void * user_data, 
+            callback_t const & callback)
+        {
+            boost::asio::deadline_timer * timer2 = new boost::asio::deadline_timer(io_svc());
+            timer2->expires_from_now(boost::posix_time::milliseconds(delay));
+            timer2->async_wait(boost::bind(callback, user_data, _1));
+            return timer2;
+        }
+
+        void ScheduleManager::cancel_callback(
+            void * handle, 
+            boost::system::error_code & ec)
+        {
+            boost::asio::deadline_timer * timer = (boost::asio::deadline_timer *)handle;
+            timer->cancel(ec);
+            delete timer;
+        }
+
+    } // namespace common
+} // namespace ppbox
---
+++ ScheduleManager.h
@@ -0,0 +1,43 @@
+// ScheduleManager.h
+
+#ifndef _PPBOX_COMMON_SCHEDULE_MANAGER_H_
+#define _PPBOX_COMMON_SCHEDULE_MANAGER_H_
+
+#include <util/daemon/Module.h>
+
+namespace ppbox
+{
+    namespace common
+    {
+
+        class ScheduleManager
+            : public util::daemon::ModuleBase<ScheduleManager>
+        {
+        public:
+            typedef boost::function<
+                void (void *, boost::system::error_code const & ec)> callback_t;
+
+        public:
+            ScheduleManager(
+                util::daemon::Daemon & daemon);
+
+        public:
+            virtual boost::system::error_code startup();
+
+            virtual void shutdown();
+
+        public:
+            void * schedule_callback(
+                boost::uint32_t delay, // ms
+                void * user_data, 
+                callback_t const & callback);
+
+            void cancel_callback(
+                void * handle, 
+                boost::system::error_code & ec);
+        };
+
+    } // namespace common
+} // namespace ppbox
+
+#endif // _PPBOX_COMMON_SCHEDULE_MANAGER_H_
