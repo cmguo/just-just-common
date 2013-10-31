f3f5aff5f0081d69dcb0541940838d4b8a772d46
isxxguo <isxxguo@pptv.com>
2013-10-31 03:51:35 +0000

    /ppbox/branches/1.2.0 ppbox/ 增加 Blob 相关接口，调整接口所在文件 emailto: box-dev
    git-svn-id: http://svn01/repositories/r01/ppbox/branches/1.2.0@7607 b3a1389e-2159-5c43-9038-463bf877d0d9

---
+++ BlobManager.cpp
@@ -0,0 +1,78 @@
+// BlobManager.cpp
+
+#include "ppbox/common/Common.h"
+#include "ppbox/common/BlobManager.h"
+
+namespace ppbox
+{
+    namespace common
+    {
+
+        struct BlobManager::Blob
+        {
+            Blob(
+                boost::asio::const_buffer const & data, 
+                bool remove_on_get)
+            {
+                boost::uint8_t const * buf = 
+                    boost::asio::buffer_cast<boost::uint8_t const *>(data);
+                size_t size = boost::asio::buffer_size(data);
+                this->data.assign(buf, buf + size);
+                this->remove_on_get = remove_on_get;
+            }
+            std::vector<boost::uint8_t> data;
+            bool remove_on_get;
+        };
+
+        BlobManager::BlobManager()
+        {
+        }
+
+        boost::system::error_code BlobManager::insert(
+            std::string const & key, 
+            boost::asio::const_buffer const & data, 
+            bool remove_on_get)
+        {
+            typedef std::map<std::string, Blob *>::iterator iterator;
+            std::pair<iterator, bool> result =  blobs_.insert(
+                std::make_pair(key, new Blob(data, remove_on_get)));
+            return result.second 
+                ? boost::system::error_code() 
+                : framework::system::logic_error::item_already_exist;
+        }
+
+        boost::system::error_code BlobManager::get(
+            std::string const & key, 
+            boost::asio::const_buffer & data)
+        {
+            std::map<std::string, Blob *>::iterator iter = blobs_.find(key);
+            if (iter == blobs_.end()) {
+                return framework::system::logic_error::item_not_exist;
+            } else {
+                if (iter->second->remove_on_get) {
+                    cache_.swap(iter->second->data);
+                    data = boost::asio::buffer(cache_);
+                    delete iter->second;
+                    blobs_.erase(iter);
+                } else {
+                    data = boost::asio::buffer(iter->second->data);
+                }
+                return boost::system::error_code();
+            }
+        }
+
+        boost::system::error_code BlobManager::remove(
+            std::string const & key)
+        {
+            std::map<std::string, Blob *>::iterator iter = blobs_.find(key);
+            if (iter == blobs_.end()) {
+                return framework::system::logic_error::item_not_exist;
+            } else {
+                delete iter->second;
+                blobs_.erase(iter);
+                return boost::system::error_code();
+            }
+        }
+
+    } // namespace common
+} // namespace ppbox
---
+++ BlobManager.h
@@ -0,0 +1,40 @@
+// BlobManager.h
+
+#ifndef _PPBOX_COMMON_BLOB_MANAGER_H_
+#define _PPBOX_COMMON_BLOB_MANAGER_H_
+
+#include <boost/asio/buffer.hpp>
+
+namespace ppbox
+{
+    namespace common
+    {
+
+        class BlobManager
+        {
+        public:
+            BlobManager();
+
+        public:
+            boost::system::error_code insert(
+                std::string const & key, 
+                boost::asio::const_buffer const & data, 
+                bool remove_on_get);
+
+            boost::system::error_code get(
+                std::string const & key, 
+                boost::asio::const_buffer & data);
+
+            boost::system::error_code remove(
+                std::string const & key);
+
+        private:
+            struct Blob;
+            std::map<std::string, Blob *> blobs_;
+            std::vector<boost::uint8_t> cache_;
+        };
+
+    } // namespace common
+} // namespace ppbox
+
+#endif // _PPBOX_COMMON_BLOB_MANAGER_H_
